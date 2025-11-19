657b7b94b5928bc35b8c254708b0008ce9461aa2
Himanshu-Parangat <himanshuparangat005@gmail.com>
2025-11-19 06:21:22 +0530

    feat(db): add initial drizzle schema with core tables
    - added sqlite schema definitions for identity, events,
    - questions, options, participants, submissions, and responses
    - defined foreign keys and boolean fields using drizzle sqlite-core
    - introduced initial relations for dbIdentity -> event

---
+++ src/db/schema.ts
@@ -0,0 +1,94 @@
+import { relations } from "drizzle-orm";
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+
+
+export const dbIdentity = sqliteTable("dbIdentity", {
+  dbId: text("dbId").primaryKey().$default(() => "singleton"),
+  dbLabel: text("dbLabel"),
+  dbDataCreatedAt: text("dbDataCreatedAt"),
+  dbTitle: text("dbTitle"),
+  dbDescription: text("dbDescription"),
+});
+
+
+export const event = sqliteTable("event", {
+	eventId: text("eventId").primaryKey(),
+	eventlabel: text("eventlabel"),
+	eventTitle: text("eventTitle"),
+	eventCreatedAt: text("eventCreatedAt"),
+	eventDescription: text("eventDescription"),
+	eventScheduledStartAt: text("eventScheduledStartAt"),
+	eventScheduledEndAt: text("eventScheduledEndAt"),
+	eventIsPinned: integer("eventIsPinned", {mode: "boolean"}).notNull().default(false),
+
+	dbId: text("dbId").notNull().references(() => dbIdentity.dbId),
+})
+
+
+
+
+export const question = sqliteTable("question", {
+	questionId: text("questionId").primaryKey(),
+	questionlabel: text("questionlabel"),
+	questionCreatedAt: text("questionCreatedAt"),
+	questionTitle: text("questionTitle"),
+	questionDescription: text("questionDescription"),
+	questionType: text("questionType", {enum: ["checkbox","radio"]}),
+	questionIsRequired: integer("questionIsRequired", {mode: "boolean"}),
+	questionOrder: integer("questionOrder"),
+
+	eventId: text("eventId").notNull().references(() => event.eventId)
+})
+
+
+export const option = sqliteTable("option", {
+	optionId: text("optionId").primaryKey(),
+	optionlabel: text("optionlabel"),
+	optionCreatedAt: text("optionCreatedAt"),
+	optionTitle: text("optionTitle"),
+	optionOrder: integer("optionOrder"),
+	optionKey: integer("optionKey", { mode: "boolean" })
+  .notNull()
+  .default(false),
+
+	questionId: text("questionId").notNull().references(() => question.questionId)
+})
+
+
+export const participant = sqliteTable("participant", {
+	participantId: text("participantId").primaryKey(),
+	participantlabel: text("participantlabel"),
+	participantCreatedAt: text("participantCreatedAt"),
+	participantName: text("participantName"),
+	participantPayrollId: text("participantPayrollId"),
+	participantDepartment: text("participantDepartment"),
+	participantEmail: text("participantEmail"),
+	participantPhone: text("participantPhone"),
+})
+
+// user submitting there submission,
+export const submission = sqliteTable("submission", {
+	submissionId: text("submissionId").primaryKey(),
+	submissionCreatedAt: text("submissionCreatedAt"),
+
+	eventId: text("eventId").notNull().references(() => event.eventId),
+	participantId: text("participantId").notNull().references(() => participant.participantId),
+})
+
+
+export const response = sqliteTable("response", {
+	responseId: text("responseId").primaryKey(),
+	responseCreatedAt: text("responseCreatedAt"),
+
+	questionId: text("questionId").notNull().references(() => question.questionId),
+	optionId: text("optionId").notNull().references(() => option.optionId),
+  submissionId: text("submissionId").notNull().references(() => submission.submissionId),
+})
+
+
+// --- Relations Definitions ---
+
+export const dbIdentityRelations = relations(dbIdentity, ({ many }) => ({
+  events: many(event),
+}));
+//
